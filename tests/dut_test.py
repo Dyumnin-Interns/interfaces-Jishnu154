18e5c0ae81b7de343df5c21ea0f5c582a32dff64
Jishnu P <jishnupanicker2003@gmail.com>
2025-06-18 14:26:29 +0000

    Updated dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,35 +1,47 @@
 import cocotb
-from cocotb.triggers import RisingEdge, Timer
 from cocotb.clock import Clock
-
+from cocotb.triggers import RisingEdge, Timer
+from cocotb_bus.drivers import BusDriver
 
 @cocotb.test()
 async def test_dut(dut):
-
     cocotb.start_soon(Clock(dut.CLK, 10, units="ns").start())
-
     dut.RST_N.value = 0
-    await Timer(20, units="ns")
+    await Timer(20, units='ns')
     dut.RST_N.value = 1
-    await Timer(20, units="ns")
+    await Timer(20, units='ns')
+    write_drv = Wbus(dut, "", dut.CLK)
+    read_drv = Rbus(dut, "", dut.CLK)
+    await write_drv.write(4, 1)
+    await write_drv.write(5, 1)
 
-    dut.write_address.value = 4
-    dut.write_data.value = 1
-    dut.write_en.value = 1
-    await RisingEdge(dut.CLK)
-    dut.write_en.value = 0
+class Wbus(BusDriver):
+    _signals = ["write_address", "write_data", "write_en"]
 
-    dut.write_address.value = 5
-    dut.write_data.value = 1
-    dut.write_en.value = 1
-    await RisingEdge(dut.CLK)
-    dut.write_en.value = 0
+    def __init__(self, dut, name, clock):
+        BusDriver.__init__(self,dut, name, clock)
+        self.bus.write_address.value=0
+        self.bus.write_data.value=0
+        self.bus.write_en.value=0
 
-    for _ in range(10):
-        await RisingEdge(dut.CLK)
+    async def write(self, address, data):
+        self.bus.write_address.value = address
+        self.bus.write_data.value = data
+        self.bus.write_en.value = 1
+        await RisingEdge(self.clock)
+        self.bus.write_en.value = 0
 
-    dut.read_address.value = 3
-    dut.read_en.value = 1
-    await RisingEdge(dut.CLK)
-    dut.read_en.value = 0
+class Rbus(BusDriver):
+    _signals = ["read_address", "read_en"]
 
+    def __init__(self, dut, name, clock):
+        BusDriver.__init__(self,dut, name, clock)
+        self.bus.read_address.value=0
+        self.bus.read_en.value=0
+
+    async def read(self, address):
+        self.bus.read_address.value = address
+        self.bus.read_en.value = 1
+        await RisingEdge(self.clock)
+        data = int(self.entity.read_data.value)
+        self.bus.read_en.value = 0
